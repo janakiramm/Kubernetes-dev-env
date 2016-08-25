6f93fc8fd942d22791e4ee7e2dd64540eba82501
Janakiram MSV <janakiramm@gmail.com>
2016-08-25 20:08:20 +0530

    Added a comment

--- step3-kubernetes-dev.sh
+++ step3-kubernetes-dev.sh
@@ -1,3 +1,6 @@
+# Make sure we have the IP of the docker machine hosting the registry
+REG_IP=`docker-machine ip registry`
+
 # Create the minikube single node cluster pointing it to the local registry
 minikube start --vm-driver="virtualbox" --insecure-registry="$REG_IP":80
 
